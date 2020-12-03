e1ea405ec47ebd1effe5134298c236a54cc46695
Nicolas Vallotton <nvallott@hotmail.com>
2020-12-03 19:50:56 +0100

    add sample and sample button + new js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Anasol - Nicolas Vallotton - Décembre 2020
+let nSample = 1;
+console.log("hidfsd");
+let newSample = function(){
+  let ele = document.getElementById("new_sample");
+  let newForm = document.getElementById("form_sample").innerHTML;
+  document.getElementById("more_sample").remove();
+  ele.innerHTML += newForm;
+}
