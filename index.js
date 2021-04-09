a2781da580130fe7cb53c846caab74bfdd4c77d2
Nicolas Vallotton <nvallott@hotmail.com>
2021-04-09 09:14:04 +0200

    many changes, new map, popup. getParcelle

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // Anasol - Nicolas Vallotton - Décembre 2020
-let nSample = 1;
-console.log("hidfsd");
+
 let newSample = function(){
   let ele = document.getElementById("new_sample");
   let newForm = document.getElementById("form_sample").innerHTML;
   document.getElementById("more_sample").remove();
   ele.innerHTML += newForm;
+}
+
+let getParcelle = function(){
+  let item = document.getElementsByClassName("esri-feature-content").item(0).innerText
+  console.log(item);
 }
